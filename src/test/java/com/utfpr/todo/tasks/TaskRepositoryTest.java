8acc8e1a07c064677f3bc7dc0bcbc6a75c0bd6b4
Adriano Ronszcka <ronszcka@Adrianos-MacBook-Pro.local>
2024-03-23 14:06:05 -0300

    date format for assetions

--- src/test/java/com/utfpr/todo/tasks/TaskRepositoryTest.java
+++ src/test/java/com/utfpr/todo/tasks/TaskRepositoryTest.java
@@ -1,10 +1,10 @@
 package com.utfpr.todo.tasks;
 
+import java.time.format.DateTimeFormatter;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -13,35 +13,22 @@ public class TaskRepositoryTest {
   @Autowired
   private TaskRepository taskRepository;
 
-  //@Autowired
-  //private TestEntityManager testEntityManager;
-
   @Test
   public void createTask_WithDataValid_ReturnsTask() {
 
     TaskModel createdTask = taskRepository.save(TaskConstants.TASK);
 
-    System.out.println("createdTask");
-    System.out.println(createdTask);
-
-    //TaskModel foundTask = testEntityManager.find(TaskModel.class, createdTask.getId());
-
     TaskModel foundTask = taskRepository.findById(createdTask.getId()).get();
 
-    System.out.println("foundTask");
-    System.out.println(foundTask);
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
-    System.out.println(createdTask.getStartAt().toLocalDate());
-    System.out.println(foundTask.getStartAt().toLocalDate());
-
-    //Assertions.assertThat(createdTask).isEqualTo(foundTask);
     Assertions.assertThat(createdTask).isNotNull();
     Assertions.assertThat(createdTask.getId()).isNotNull();
     Assertions.assertThat(createdTask.getTitle()).isEqualTo(foundTask.getTitle());
     Assertions.assertThat(createdTask.getDescription()).isEqualTo(foundTask.getDescription());
     Assertions.assertThat(createdTask.getPriority()).isEqualTo(foundTask.getPriority());
-    Assertions.assertThat(createdTask.getStartAt().toLocalDate()).isEqualTo(foundTask.getStartAt().toLocalDate());
-    Assertions.assertThat(createdTask.getEndAt().toLocalDate()).isEqualTo(foundTask.getEndAt().toLocalDate());
+    Assertions.assertThat(createdTask.getStartAt().format(formatter)).isEqualTo(foundTask.getStartAt().format(formatter));
+    Assertions.assertThat(createdTask.getEndAt().format(formatter)).isEqualTo(foundTask.getEndAt().format(formatter));
 
   }
 
